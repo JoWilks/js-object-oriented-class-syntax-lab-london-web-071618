bd11376e678a1f969b8611967f7dc11219e0b40f
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-09-02 11:49:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 class BoardMember {
   
-  constructor
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
   
   
 }
